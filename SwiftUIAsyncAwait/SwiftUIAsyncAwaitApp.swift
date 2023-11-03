a1fa646ef0ea1ad7fd0ff4efbfe6f23f3de5828d
Vladimir Tezin <vtezin@gmail.com>
2023-11-03 06:38:15 +0100

    Initial Commit

---
+++ SwiftUIAsyncAwait/SwiftUIAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncAwaitApp.swift
+//  SwiftUIAsyncAwait
+//
+//  Created by Vladimir Tezin on 03.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
